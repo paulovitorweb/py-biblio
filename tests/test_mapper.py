63cf8cbdddd9d2690f4670d975a5669fd568a53b
Paulo Freitas <paulogeo5@gmail.com>
2022-04-04 00:31:35 -0300

    test: add test for mapper relationships

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -1,7 +1,8 @@
-from src.pybiblio.models import Author
+from sqlalchemy.orm import Session
+from src.pybiblio.models import Author, Book
 
 
-def test_author_mapper_can_load_authors(session):
+def test_author_mapper_can_load_authors(session: Session):
     session.execute(
         'INSERT INTO authors (name) VALUES '
         '("Paulo de Freitas"),'
@@ -14,9 +15,21 @@ def test_author_mapper_can_load_authors(session):
     assert session.query(Author).all() == expected
 
 
-def test_author_mapper_can_save_authors(session):
+def test_author_mapper_can_save_authors(session: Session):
     new_author = Author(name='Paulo de Freitas')
     session.add(new_author)
     session.commit()
     rows = list(session.execute('SELECT id, name FROM "authors"'))
     assert rows == [(1, 'Paulo de Freitas')]
+
+
+def test_author_book_mapper_can_get_relationship(session: Session):
+    new_book = Book(None, 'Ecossistema GIS', 2020, [Author(name='Paulo de Freitas')], 'João Pessoa', 'Wordpress')
+    session.add(new_book)
+    session.commit()
+
+    book = session.query(Book).filter_by(id=1).one()
+    assert book.authors == [Author(1, 'Paulo de Freitas')]
+
+    author = session.query(Author).filter_by(id=1).one()
+    assert author.books == [book]

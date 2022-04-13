a274b22d3834d018c5fecfd3a653193d446dff17
Paulo Freitas <paulogeo5@gmail.com>
2022-04-13 00:02:50 -0300

    feat: add view to create author

--- src/pybiblio/api/views.py
+++ src/pybiblio/api/views.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException
 from src.pybiblio.infrastructure import repository
 from src.pybiblio.infrastructure.orm import Session
+from src.pybiblio.domain.models import Author
+from . import schemas
 
 
 def get_author(db: Session, author_id: int):
@@ -8,3 +10,11 @@ def get_author(db: Session, author_id: int):
     if author is None:
         raise HTTPException(404, f'Author with id {author_id} not found')
     return author
+
+
+def create_author(db: Session, author: schemas.AuthorCreate):
+    db_author = Author(name=author.name)
+    repository.AuthorRepository(session=db).add(db_author)
+    db.commit()
+    db.refresh(db_author)
+    return db_author
--- tests/test_views.py
+++ tests/test_views.py
@@ -3,6 +3,7 @@ from fastapi import HTTPException
 from src.pybiblio.infrastructure import repository
 from src.pybiblio.domain.models import Author
 from src.pybiblio.api import views
+from src.pybiblio.api import schemas
 
 
 def test_get_author_should_succeed(mocker):
@@ -16,3 +17,20 @@ def test_get_author_should_raise_error(mocker):
     mocker.patch.object(repository.AuthorRepository, 'get', return_value=None)
     with pytest.raises(HTTPException):
         views.get_author(db, 1)
+
+
+def test_create_author_should_succeed(mocker):
+    db = mocker.MagicMock()
+    author_create = schemas.AuthorCreate(name='Son Goku')
+
+    def refresh_author(author):
+        author.id = 2
+
+    db.refresh.side_effect = refresh_author
+    add_method = mocker.patch.object(repository.AuthorRepository, 'add')
+
+    author = views.create_author(db, author_create)
+    add_method.assert_called_once()
+    db.commit.assert_called_once()
+    db.refresh.assert_called_once()
+    assert author == Author(2, name='Son Goku')

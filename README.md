5ce74062fcfaab2c4182c6e93ecd6610eb4bea40
Paulo Freitas <paulogeo5@gmail.com>
2022-04-15 16:16:50 -0300

    docs: add custom openapi attributes

--- README.md
+++ README.md
@@ -1,23 +1,35 @@
 # py-biblio
+
 Uma aplicação escrita em Python com dataclasses para gerenciar referências bibliográficas.
 
 ## Build
 
 ### Requisitos
+
 - Python >= 3.8
 
 ### Crie um ambiente virtual e ative
+
 ```
 python3 -m venv venv
 source venv/bin/activate
 ```
 
 ### Instale as dependências
+
 ```
 pip install -r requirements.txt
 ```
 
+### Documentação
+
+Para acessar a documentação da API:
+
+- Com Swagger: http://127.0.0.1:8000/docs
+- Com ReDoc: http://127.0.0.1:8000/redoc
+
 ### Teste
+
 ```
 pytest
 ```
--- src/pybiblio/api/main.py
+++ src/pybiblio/api/main.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import Depends, FastAPI
+from fastapi.openapi.utils import get_openapi
 from sqlalchemy.orm import Session
 
 from src.pybiblio.infrastructure.orm import DbSession, init_db
@@ -49,3 +50,19 @@ def read_book(book_id: int, db: Session = Depends(get_db)):
 @app.post('/books/', response_model=schemas.Book)
 def create_book(book: schemas.BookCreate, db: Session = Depends(get_db)):
     return views.create_book(db, book=book)
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title='PyBiblio',
+        version='0.1.0',
+        description='Uma aplicação escrita em Python para gerenciar referências bibliográficas.',
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
